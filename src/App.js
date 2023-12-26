573d93d67f717ccbb0c6060a4292dd7e0f725cf9
michael-a7 <michael.albo1@yahoo.com>
2023-12-25 17:15:58 -0800

    Add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import {Header} from './components/Header';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
 import './App.css';
 function App() {
   return (
     <div>
       <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+      </div>
     </div>
   );
 }
