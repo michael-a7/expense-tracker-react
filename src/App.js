40f7c559d1c8e4894501ab259844d5015f864031
michael-a7 <michael.albo1@yahoo.com>
2023-12-27 16:14:07 -0800

    AddTransaction and TransactionList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 import './App.css';
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div className='container'>
         <Balance/>
         <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
     </div>
   );
